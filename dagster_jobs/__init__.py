78ce1f365cc5d66c2ffd1b1dca5cccec19983515
Evan Zamir <zamir.evan@gmail.com>
2024-12-05 05:48:17 -0800

    --strength of schedule and more

--- dagster_jobs/__init__.py
+++ dagster_jobs/__init__.py
@@ -2,13 +2,15 @@ from dagster import Definitions, load_assets_from_modules, with_source_code_refe
 
 from .assets import espn, women, models
 from .resources import database_resource, download_base_path, jinja_templates_path
-from .jobs import daily_update_job, daily_update_job_women, cleanup_job, models_update_job, top_lines_job, season_report_job
+from .jobs import daily_update_job, daily_update_job_women, cleanup_job, models_update_job, models_update_job_women, \
+    top_lines_job, top_lines_job_women, season_report_job, season_report_job_women
 from .schedules import daily_update_schedule
 
 espn_assets = load_assets_from_modules([espn, women])
 model_assets = load_assets_from_modules([models])
 
-all_jobs = [daily_update_job, daily_update_job_women, models_update_job, top_lines_job, cleanup_job, season_report_job]
+all_jobs = [daily_update_job, daily_update_job_women, models_update_job, models_update_job_women, 
+            top_lines_job, cleanup_job, season_report_job, top_lines_job_women, season_report_job_women]
 all_schedules = [daily_update_schedule]
 
 defs = Definitions(
